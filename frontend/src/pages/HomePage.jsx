c7265204559d0aebd20521909579dc44d62bc0eb
Andjela Scepovic <scepovic.andjela@gmail.com>
2024-12-26 22:26:27 +0100

    fixed button navig

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import greenChair from "../assets/greenChair.png";
 
 function HomePage() {
+  const navigate = useNavigate();
+
   function products() {
     navigate("/products");
   }
