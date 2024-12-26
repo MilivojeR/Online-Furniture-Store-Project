e9f52848ed15a15229e6499da0e1dd9ee84741e2
Andjela Scepovic <scepovic.andjela@gmail.com>
2024-12-26 22:22:39 +0100

    made some changes on Home

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import greenChair from "../assets/greenChair.png";
 
 function HomePage() {
-  return <div></div>;
+  function products() {
+    navigate("/products");
+  }
+
+  return (
+    <div className="bg-gradient-to-b from-amber-400 to-lime-950 flex items-center justify-between">
+      <div className="text-white flex-col justify-center ml-20">
+        <p className="text-4xl">
+          Organize with <br /> Style
+        </p>
+        <p>A new collection of desk chairs is available for purchase.</p>
+        <button className="btn btn-danger" onClick={() => products()}>
+          Shop Now
+        </button>
+      </div>
+      <div>
+        <img src={greenChair} alt="Green Chair" />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
